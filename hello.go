45a3394c74770c021829a6130af9a70088a5490e
David Peterson <david@randombits.org>
2016-03-06 00:22:51 -0300

    * Fixed URL for the 'hello' query.

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ package contegixclassic
 func (c *Client) SayHello() (*string, error) {
 	var body *string
 
-	_, err := c.DoRequest("GET", "/virtual_machines", nil, &body)
+	_, err := c.DoRequest("GET", "/hello", nil, &body)
 
 	return body, err
 }
